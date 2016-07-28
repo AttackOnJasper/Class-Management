9398fba4d4f497f82ceacc94eb4f98f1f1393a38
Jasper Wang <jasperw@ca.ibm.com>
2016-07-28 11:43:42 -0400

    init app settings

---
+++ Class-Management/AppSettings.swift
@@ -0,0 +1,13 @@
+//
+//  AppSetting.swift
+//  Class-Management
+//
+//  Created by Jasper Wang on 7/28/16.
+//  Copyright © 2016 Jasper Wang. All rights reserved.
+//
+
+import Foundation
+
+class AppSettings {
+
+}
